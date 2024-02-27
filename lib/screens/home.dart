3c4834af53007335958852188974f2c924835c40
Venka <duraivenkat19@gmail.com>
2024-02-28 01:23:03 +0530

    Implemented CRUD

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,6 +12,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+
+  List<Contact> contacts = List.empty(growable: true);
+
+  int selectedIndex = -1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,22 +29,68 @@ class _HomePageState extends State<HomePage> {
           ),
           Container(
             padding: EdgeInsets.all(20),
-            child: const TextField(
-              decoration: InputDecoration(labelText: "Enter reminder"),
+            child: TextField(
+              controller: nameController,
+              decoration: const InputDecoration(labelText: "Enter Name"),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            child: TextField(
+              controller: addressController,
+              decoration: const InputDecoration(labelText: "Enter Address"),
             ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  String name = nameController.text;
+                  String address = addressController.text;
+                  if (name.isNotEmpty && address.isNotEmpty) {
+                    setState(() {
+                      nameController.text = " ";
+                      addressController.text = " ";
+                      contacts.add(Contact(name: name, address: address));
+                    });
+                  }
+                },
                 child: Text("Save"),
               ),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  String name = nameController.text;
+                  String address = addressController.text;
+                  if (name.isNotEmpty && address.isNotEmpty) {
+                    setState(
+                      () {
+                        nameController.text = " ";
+                        addressController.text = " ";
+                        contacts[selectedIndex].name = name;
+                        contacts[selectedIndex].address = address;
+                        selectedIndex = -1;
+                      },
+                    );
+                  }
+                },
                 child: Text("Update"),
               ),
             ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          contacts.isEmpty
+              ? const Text("No contacts")
+              : Expanded(
+                  child: ListView.builder(
+                    itemCount: contacts.length,
+                    itemBuilder: (context, index) => getRow(index),
+                  ),
+                ),
+          const SizedBox(
+            height: 20,
           ),
           Align(
             alignment: Alignment.center,
@@ -61,4 +114,43 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
+  Widget getRow(int index) {
+    return Card(
+      child: ListTile(
+        trailing: SizedBox(
+          width: 70,
+          child: Row(
+            children: [
+              InkWell(
+                  onTap: () {
+                    nameController.text = contacts[index].name;
+                    addressController.text = contacts[index].address;
+                    setState(() {
+                      selectedIndex = index;
+                    });
+                  },
+                  child: Icon(Icons.edit)),
+              InkWell(
+                  onTap: () {
+                    setState(() {
+                      contacts.removeAt(index);
+                    });
+                  },
+                  child: Icon(Icons.delete)),
+            ],
+          ),
+        ),
+        title: Column(
+          children: [Text(contacts[index].name), Text(contacts[index].address)],
+        ),
+      ),
+    );
+  }
+}
+
+class Contact {
+  String name;
+  String address;
+  Contact({required this.name, required this.address});
 }
